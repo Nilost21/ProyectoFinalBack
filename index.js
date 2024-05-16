e07962c5fc4e406058766477db3bcdda634502a1
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-05-16 20:12:07 -0300

    comentando middleware

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ app.listen(PORT, () => {
 });
 
 // Middleware to handle 404 errors
-app.use((req, res, next) => {
-  const err = new Error(`Path not Found: ${req.method} ${req.originalUrl}`);
-  err.status = 404;
-  next(err);
-});
+// app.use((req, res, next) => {
+//   const err = new Error(`Path not Found: ${req.method} ${req.originalUrl}`);
+//   err.status = 404;
+//   next(err);
+// });
 
 // Middleware to handle errors
 app.use(errorHandler);
